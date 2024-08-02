3171b4d3249339972afb55177e0154409242a044
HagiwaraShogo <hagiwara@sieggames.co.jp>
2024-08-02 17:52:18 +0900

    added per-user data isolation

--- app/page.tsx
+++ app/page.tsx
@@ -40,8 +40,9 @@ export default function App() {
 
   return (
     <Authenticator>
-    {({ signOut, user }) => (
+     {({ signOut, user }) => (
       <main>
+        <h1>{user?.signInDetails?.loginId}'s todos</h1>
         <h1>My todos</h1>
         <button onClick={createTodo}>+ new</button>
         <ul>
